5fa6e0923c9681bab086c1543add5875435e34f2
Ivan Kent Custodio <114642839+Venersiss@users.noreply.github.com>
2022-11-09 21:46:17 -0800

    Store but diff (JOptionPane) v2

--- store/Store.java
+++ store/Store.java
@@ -1,14 +1,17 @@
-
-package store;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package custodio;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author Venersiss
+ * @author PC
  */
 public class Store {
 
@@ -16,95 +19,108 @@ public class Store {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // TODO code application logic here
-        Scanner a = new Scanner(System.in);
+        // TODO code application logic here
+
         Map m = new HashMap();
-        JOptionPane.showMessageDialog(null,  "Uy choi!");
+        JOptionPane.showMessageDialog(null, "Uy choi!");
+        JOptionPane.showMessageDialog(null, "▬▬ι═══════ﺤ");
+        JOptionPane.showMessageDialog(null, "⁽⁽(੭ꐦ •̀Д•́ )੭*⁾⁾");
+        JOptionPane.showMessageDialog(null, "╭∩╮( •̀_•́ )╭∩╮");
+        JOptionPane.showMessageDialog(null, "( -_•)╦̵̵̿╤─");
+        JOptionPane.showMessageDialog(null, "(ง'̀-'́)ง");
         m.put("spada", 15);
         m.put("pana", 10);
         m.put("elixer", 20);
         m.put("shield", 25);
         m.put("ring", 30);
-        String b;
-        String f = null;
-        String pila;
-        int bayad;
-        int kulang;
-        int asura;
-        int d = 0;
-        int v = 0;
-        int c = 0;
-        int total;
-        int total2 = 0;
-        int total3 = 0;
+        String [] i = {"spada","pana","elixer","shield","ring"};
+        String[] y = {" ", " ", " "};
+        float[] x = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] w = {0};
         do {
-            total3=0;
-                v=0;
-                total=0;
-                total2=0;
+            x[5] = 0;
+            x[1] = 0;
+            x[3] = 0;
+            x[4] = 0;
             do {
-                           
-                b = JOptionPane.showInputDialog("spada    :15 " + "\n" + "pana     :10 " + "\n" + "elixer   :20 " + "\n" + "shield   :25 " + "\n" + "ring     :30 " + "\n"+"unsa man imoha dong");
-                if (m.containsKey(b)) {int z = (int) m.get(b);
-                    
-                    pila = JOptionPane.showInputDialog("how many?");
-                    c=Integer.parseInt(pila);
-                    total = z * c;
-                    v = c + d;
-                    total3 = total + total2;
-                    JOptionPane.showMessageDialog(null, "total is " + total3);
-                    total2 = total3;
-                    d = c;
-                    while (true) {
-                        
-                        f = JOptionPane.showInputDialog("mamalit paka or dili na?" + "\n" + "[mamalit] or [dili]");
-                        if (f.equals("mamalit") || f.equals("dili")) {  break;}
-                    }
-                } 
-            } while (f.equals("mamalit"));
-            if (f.equals("dili")) {
-                if (v >= 10 && v < 20) {
-                    JOptionPane.showMessageDialog(null, "kay " + v + " man kabuok imong item gipalit naa kay libre nga 10 healing potions!");
-                } else if (v >= 20) { System.out.println("kay " + v + " man kabuok imong item gipalitnaa kay libre 10 healing potions ug 10 mana potions!");}
-                
-                JOptionPane.showMessageDialog(null, "Ang imo bayronon kay " + total2);
-                
-                pila = JOptionPane.showInputDialog("member ka sa asura guild? unsa na rank?");
-                asura=Integer.parseInt(pila);
-                switch (asura) {
-                    case 3:
-                        JOptionPane.showMessageDialog(null, "naa kay 10% discount");
-                        total3 = (int) (total3 * 0.90);
-                        break;
-                    case 2:
-                       JOptionPane.showMessageDialog(null, "naa kay 20% discount");
-                        total3 = (int) (total3 * 0.80);
-                        break;
-                    case 1:
-                        JOptionPane.showMessageDialog(null, "naa kay 30% discount");
-                        total3 = (int) (total3 * 0.70);
-                        break;
-                    default:
-                        JOptionPane.showMessageDialog(null, "wala kay discount");
-                        break;}
 
-                JOptionPane.showMessageDialog(null,"ang imo bayronon kay " + total3);
-               pila=JOptionPane.showInputDialog("palihog butang sa imo bayad");
-                bayad = Integer.parseInt(pila);
-                do {if (bayad < total3) {
-                        kulang = (total3 - bayad);
-                        JOptionPane.showMessageDialog(null ,"kulang man imo gi bayad ang kulang kay: " + kulang);
-                        pila=JOptionPane.showInputDialog("palihog ko dungag");
-                        int dungag = Integer.parseInt(pila);
-                        bayad = bayad + dungag;}  
-                } while (bayad < total3);
-                if (bayad >= total3) {int kambyo = (bayad - total3);
-                    JOptionPane.showMessageDialog(null ,"imo kambyo kay " + kambyo);
-                   pila = JOptionPane.showInputDialog("For the next customer press 1");
-                    c = Integer.parseInt(pila);}
-                
+                y[0] = JOptionPane.showInputDialog("spada    :15 " + "\n" + "pana     :10 " + "\n" + "elixer   :20 " + "\n" + "shield   :25 " + "\n" + "ring     :30 " + "\n" + "unsa man imoha dong");
+                if (m.containsKey(y[0])) {
+                    float z = (int) m.get(y[0]);
+
+                    y[2] = JOptionPane.showInputDialog("how many?");
+                    x[2] = Float.parseFloat(y[2]);
+                    x[3] = z * x[2];
+                    x[1] = x[2] + x[0];
+                    x[5] = x[3] + x[4];
+                    JOptionPane.showMessageDialog(null, "total is " + x[5]);
+                    x[4] = x[5];
+                    x[0] = x[2];
+                    while (true) {
+
+                        y[1] = JOptionPane.showInputDialog("mamalit paka or dili na?" + "\n" + "[mamalit] or [dili]");
+                        if (y[1].equals("mamalit") || y[1].equals("dili")) {
+                            break;
+                        }
+                    }
+                }
+            } while (y[1].equals("mamalit"));
+            if (y[1].equals("dili")) {
+                if (x[1] >= 10 && x[1] < 20) {
+                    JOptionPane.showMessageDialog(null, "kay " + x[1] + " man kabuok imong item gipalit naa kay libre nga 10 healing potions!");
+                } else if (x[1] >= 20) {
+                    System.out.println("kay " + x[1] + " man kabuok imong item gipalitnaa kay libre 10 healing potions ug 10 mana potions!");
+                }
+
+                JOptionPane.showMessageDialog(null, "Ang imo bayronon kay " + x[4]);
+                do {
+                    y[2] = JOptionPane.showInputDialog("member ka sa asura guild? unsa na rank?");
+                    w[0] = Integer.parseInt(y[2]);
+                    switch (w[0]) {
+
+                        case 3:
+                            JOptionPane.showMessageDialog(null, "naa kay 10% discount");
+                            x[5] = (float) (x[5] * 0.90);
+                            break;
+                        case 2:
+                            JOptionPane.showMessageDialog(null, "naa kay 20% discount");
+                            x[5] =  (float) (x[5] * 0.80);
+                            break;
+                        case 1:
+                            JOptionPane.showMessageDialog(null, "naa kay 30% discount");
+                            x[5] =  (float) (x[5] * 0.70);
+                            break;
+                        case 0:
+                            JOptionPane.showMessageDialog(null, "wala kay discount");
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "The Asura guild does not have that rank");
+                            x[9] = 1;
+                            break;
+                    }
+                } while (x[9]==1);
+
+                JOptionPane.showMessageDialog(null, "ang imo bayronon kay " + x[5]);
+                y[2] = JOptionPane.showInputDialog("palihog butang sa imo bayad");
+                x[6] = Float.parseFloat(y[2]);
+                do {
+                    if (x[6] < x[5]) {
+                        x[7] = (x[5] - x[6]);
+                        JOptionPane.showMessageDialog(null, "kulang man imo gi bayad ang kulang kay: " + x[7]);
+                        y[2] = JOptionPane.showInputDialog("palihog ko dungag");
+                        float dungag = Float.parseFloat(y[2]);
+                        x[6] = x[6] + dungag;
+                    }
+                } while (x[6] < x[5]);
+                if (x[6] >= x[5]) {
+                    float kambyo = (x[6] - x[5]);
+                    JOptionPane.showMessageDialog(null, "imo kambyo kay " + kambyo);
+                    y[2] = JOptionPane.showInputDialog("For the next customer press 1");
+                    x[2] = Float.parseFloat(y[2]);
+                }
+
             }
-        } while (c == 1);
+        } while (x[2] == 1);
     }
-    
+
 }

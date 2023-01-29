663508494a45af5471b3f52dfc3488a690f50419
Ivan Kent Custodio <114642839+Venersiss@users.noreply.github.com>
2023-01-29 23:51:30 +0800

    Store but diff (JOptionPane) v3

--- store/Store.java
+++ store/Store.java
@@ -1,9 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package custodio;
+
+package com.mycompany.store_joption;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,31 +10,22 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author PC
+ * @author Venersis
  */
 public class Store {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
 
-        Map m = new HashMap();
-        JOptionPane.showMessageDialog(null, "Uy choi!");
-        JOptionPane.showMessageDialog(null, "▬▬ι═══════ﺤ");
-        JOptionPane.showMessageDialog(null, "⁽⁽(੭ꐦ •̀Д•́ )੭*⁾⁾");
-        JOptionPane.showMessageDialog(null, "╭∩╮( •̀_•́ )╭∩╮");
-        JOptionPane.showMessageDialog(null, "( -_•)╦̵̵̿╤─");
-        JOptionPane.showMessageDialog(null, "(ง'̀-'́)ง");
-        m.put("spada", 15);
-        m.put("pana", 10);
-        m.put("elixer", 20);
-        m.put("shield", 25);
-        m.put("ring", 30);
-        String [] i = {"spada","pana","elixer","shield","ring"};
+        Map m = new HashMap();       
+        m.put("tshirt", 250);
+        m.put("trouser", 300);
+        m.put("jacket", 500);
+        m.put("shoes", 1500);
+        m.put("hoodie", 450);
+        String [] i = {"Oxygen T shirt","Oxygen Trouser","Penshoppe Jacket","Penshoppe Hoodie","Nike Shoes"};
         String[] y = {" ", " ", " "};
-        float[] x = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        float[] x = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0};
         int[] w = {0};
         do {
             x[5] = 0;
@@ -43,84 +33,84 @@ public class Store {
             x[3] = 0;
             x[4] = 0;
             do {
-
-                y[0] = JOptionPane.showInputDialog("spada    :15 " + "\n" + "pana     :10 " + "\n" + "elixer   :20 " + "\n" + "shield   :25 " + "\n" + "ring     :30 " + "\n" + "unsa man imoha dong");
+               
+                y[0] = JOptionPane.showInputDialog("xygen T shirt                    :250 " + "\n" + "Oxygen Trouser              :300 " + "\n" + "Penshoppe Jacket         :500 " + "\n" + "Penshoppe Hoodie         :450 " + "\n" + "Nike Shoes                       :1500 " + "\n" + "What Is Your Order? \n \n \n [tshirt] [trouser] [jacket] [hoodie] [shoes]");
                 if (m.containsKey(y[0])) {
                     float z = (int) m.get(y[0]);
 
                     y[2] = JOptionPane.showInputDialog("how many?");
                     x[2] = Float.parseFloat(y[2]);
                     x[3] = z * x[2];
-                    x[1] = x[2] + x[0];
+                    x[1] = x[2] + x[1];
                     x[5] = x[3] + x[4];
                     JOptionPane.showMessageDialog(null, "total is " + x[5]);
                     x[4] = x[5];
-                    x[0] = x[2];
-                    while (true) {
+                    
+                    
 
-                        y[1] = JOptionPane.showInputDialog("mamalit paka or dili na?" + "\n" + "[mamalit] or [dili]");
-                        if (y[1].equals("mamalit") || y[1].equals("dili")) {
-                            break;
-                        }
-                    }
+                        
+                        
+                    
                 }
-            } while (y[1].equals("mamalit"));
-            if (y[1].equals("dili")) {
-                if (x[1] >= 10 && x[1] < 20) {
-                    JOptionPane.showMessageDialog(null, "kay " + x[1] + " man kabuok imong item gipalit naa kay libre nga 10 healing potions!");
-                } else if (x[1] >= 20) {
-                    System.out.println("kay " + x[1] + " man kabuok imong item gipalitnaa kay libre 10 healing potions ug 10 mana potions!");
+                y[1] = JOptionPane.showInputDialog("Do You Want To buy Again?" + "\n" + "[yes] or [no]");
+            } while (y[1].equals("yes"));
+           
+                if (x[1] >= 10 && x[1] <= 20) {
+                    JOptionPane.showMessageDialog(null, "Since You Bought " + x[1] + " Item You Get 2 Free Items Of Your Choice");
+                } 
+                if (x[1]<10){
+                                    JOptionPane.showMessageDialog(null, "Since You Bought " + x[1] + " Item Only You Get No Freebies");
+                }
+                if (x[1]>21){
+                                    JOptionPane.showMessageDialog(null, "Since You Bought " + x[1] + " Item You Get 3 Items Of Your Choice");
                 }
 
-                JOptionPane.showMessageDialog(null, "Ang imo bayronon kay " + x[4]);
+                JOptionPane.showMessageDialog(null, "Your Total Balance " + x[4]);
                 do {
-                    y[2] = JOptionPane.showInputDialog("member ka sa asura guild? unsa na rank?");
-                    w[0] = Integer.parseInt(y[2]);
-                    switch (w[0]) {
+                    y[2] = JOptionPane.showInputDialog("We Have Discounts For People Whose Age is [15-20], [30-40], and [50-60] \n \n Please Type Your Age Below");
+                    x[10]=Float.parseFloat(y[2]);
+                    
 
-                        case 3:
-                            JOptionPane.showMessageDialog(null, "naa kay 10% discount");
+                        if (x[10]>=15 && x[10]<=20){
+                            JOptionPane.showMessageDialog(null, "You Get 10% Discount");
                             x[5] = (float) (x[5] * 0.90);
-                            break;
-                        case 2:
-                            JOptionPane.showMessageDialog(null, "naa kay 20% discount");
+                        }
+                        else if (x[10]>=30 && x[10]<=40){
+                            JOptionPane.showMessageDialog(null, "You Get 20% discount");
                             x[5] =  (float) (x[5] * 0.80);
-                            break;
-                        case 1:
-                            JOptionPane.showMessageDialog(null, "naa kay 30% discount");
+                        }
+                        else if (x[10]>=50 && x[10]<=60){
+                            JOptionPane.showMessageDialog(null, "You Get 30% discount");
                             x[5] =  (float) (x[5] * 0.70);
-                            break;
-                        case 0:
-                            JOptionPane.showMessageDialog(null, "wala kay discount");
-                            break;
-                        default:
-                            JOptionPane.showMessageDialog(null, "The Asura guild does not have that rank");
-                            x[9] = 1;
-                            break;
-                    }
+                        }
+                        
+                        else{
+                            JOptionPane.showMessageDialog(null, "We Dont Have A Discount For Your Age");
+                            
+                        }
+                    
                 } while (x[9]==1);
 
-                JOptionPane.showMessageDialog(null, "ang imo bayronon kay " + x[5]);
-                y[2] = JOptionPane.showInputDialog("palihog butang sa imo bayad");
+                
+                y[2] = JOptionPane.showInputDialog("Total Payable Amount: "+x[5]+"\n \nPlease Input Payment");
                 x[6] = Float.parseFloat(y[2]);
                 do {
                     if (x[6] < x[5]) {
                         x[7] = (x[5] - x[6]);
-                        JOptionPane.showMessageDialog(null, "kulang man imo gi bayad ang kulang kay: " + x[7]);
-                        y[2] = JOptionPane.showInputDialog("palihog ko dungag");
+                        JOptionPane.showMessageDialog(null, "Insufficient Payment Please Add: " + x[7]);
+                        y[2] = JOptionPane.showInputDialog("Total Payable Amount: "+x[7]+"\n \nPlease Input Payment");
                         float dungag = Float.parseFloat(y[2]);
                         x[6] = x[6] + dungag;
                     }
                 } while (x[6] < x[5]);
                 if (x[6] >= x[5]) {
                     float kambyo = (x[6] - x[5]);
-                    JOptionPane.showMessageDialog(null, "imo kambyo kay " + kambyo);
+                    JOptionPane.showMessageDialog(null, "Your Change " + kambyo);
                     y[2] = JOptionPane.showInputDialog("For the next customer press 1");
                     x[2] = Float.parseFloat(y[2]);
                 }
-
-            }
+            
+            
         } while (x[2] == 1);
     }
-
 }
